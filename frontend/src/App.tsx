7c394401e1b12c177fbe5f34b028e3960ba7b1d4
Sujit Kumar <skr87981@gmail.com>
2024-03-18 17:08:21 +0530

    layout created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout> <p>Home Page</p> </Layout>}/>
+        <Route path="/search" element={<Layout> <p>Search Page</p> </Layout>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
